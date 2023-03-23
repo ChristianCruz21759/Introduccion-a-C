564a2e459e0290cfe2a38f6012632c6e0d957a04
ChristianCruz21759 <128733785+ChristianCruz21759@users.noreply.github.com>
2023-03-23 11:25:28 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+char name[] = "Christian Alejandro Cruz Rodas";
+int carne = 21759;
+
+printf("Mi nombre es %s y mi numero de carne es %d\n", name, carne);
+
+return 0;
+}
