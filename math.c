365ad1e200090d79dacce518dfaffda7ae1cd2ad
ChristianCruz21759 <128733785+ChristianCruz21759@users.noreply.github.com>
2023-03-23 11:27:04 -0600

    Create math.c

---
+++ math.c
@@ -0,0 +1,21 @@
+#include "math.h"
+
+int suma (int a, int b)
+{
+	return a + b;
+}
+
+int resta (int a, int b)
+{
+	return a - b;
+}
+
+int multiplicacion (int a, int b)
+{
+	return a * b;
+}
+
+int division (int a, int b)
+{
+	return a / b;
+}
