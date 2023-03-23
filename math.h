36f3830094bbd1de161a7f17a73e1327d4729a40
ChristianCruz21759 <128733785+ChristianCruz21759@users.noreply.github.com>
2023-03-23 11:26:52 -0600

    Create math.h

---
+++ math.h
@@ -0,0 +1,16 @@
+#ifndef MATH_H
+#define	MATH_H
+
+int suma (int a, int b);
+
+int resta (int a, int b);
+
+int multiplicacion (int a, int b);
+
+int division (int a, int b);
+
+
+
+#endif /*ADD_H*/
+
+#define PI 3.141592
